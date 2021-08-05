1a704d87dcdb926a6fa9886125c0eda1639ec4f0
kevinfAAA <kevin.foley21@hotmail.com>
2021-08-05 16:41:38 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,1 +1,32 @@
-# python code goes here
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+# Find Elements by ID
+
+results = soup.find(id="ResultsContainer")
+
+# Find Elements by HTML Class Name
+
+print(results.prettify())
+job_elements = results.find_all("div", class_="card-content")
+
+for job_element in job_elements:
+    print(job_element, end="\n"*2)
+
+# Extract Text From HTML Elements
+
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    print()
+
+
